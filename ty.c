9d4c42bf08939dfb64106bf4424815f426f61c86
Brad Garagan <bradgaragan@gmail.com>
2020-11-26 09:02:45 -0400

    add a -e option to evaluate an expression specified on the command line

--- ty.c
+++ ty.c
@@ -29,48 +29,54 @@ readln(void)
                 return fgets(buffer, sizeof buffer, stdin);
 }
 
-noreturn static void
-repl(void)
+static void
+execln(char *line)
 {
         static char buffer[8192];
 
-        for (char *line; line = readln(), line != NULL;) {
+        if (strspn(line, " \t\n") == strlen(line))
+                return;
 
-		if (strspn(line, " \t\n") == strlen(line))
-			continue;
+        /*
+         * Very bad.
+         */
+        if (use_readline)
+                line = realloc(line, strlen(line) + 2);
 
-                /*
-                 * Very bad.
-                 */
-                if (use_readline)
-                        line = realloc(line, strlen(line) + 2);
+        if (line[0] == ':') {
+                if (line[1] == '!')
+                        system(line + 2) || 0;
+                else if (!vm_execute_file(line + 1))
+                        fprintf(stderr, "%s\n", vm_error());
+                goto add;
 
-                if (line[0] == ':') {
-                        if (line[1] == '!')
-                                system(line + 2) || 0;
-                        else if (!vm_execute_file(line + 1))
-                                fprintf(stderr, "%s\n", vm_error());
-                        goto add;
+        }
 
-                }
+        strcat(line, "\n");
 
-                strcat(line, "\n");
+        sprintf(buffer, "print(%s);", line);
+        if (vm_execute(buffer))
+                goto add;
+        if (strstr(vm_error(), "ParseError") != NULL && vm_execute(line))
+                goto add;
 
-                sprintf(buffer, "print(%s);", line);
-                if (vm_execute(buffer))
-                        goto add;
-                if (strstr(vm_error(), "ParseError") != NULL && vm_execute(line))
-                        goto add;
-
-                fprintf(stderr, "%s\n", vm_error());
+        fprintf(stderr, "%s\n", vm_error());
 add:
-                if (use_readline) {
-                        line[strcspn(line, "\n")] = '\0';
-                        add_history(line);
-                }
+        if (use_readline) {
+                line[strcspn(line, "\n")] = '\0';
+                add_history(line);
+        }
 
 
-                fflush(stdout);
+        fflush(stdout);
+}
+
+noreturn static void
+repl(void)
+{
+
+        for (char *line; line = readln(), line != NULL;) {
+                execln(line);
         }
 
         exit(EXIT_SUCCESS);
@@ -82,18 +88,26 @@ main(int argc, char **argv)
 
         vm_init(argc, argv);
 
-        use_readline = isatty(0);
+        use_readline = isatty(0) && argc < 2;
 
         sqlite_load();
 
         if (argc <= 1)
                 repl();
-          
-        char const *file = (argc > 1 && strcmp(argv[1], "-") != 0) ? argv[1] : "/dev/stdin";
 
-        if (!vm_execute_file(file)) {
-                fprintf(stderr, "%s\n", vm_error());
-                return -1;
+        if (strcmp(argv[1], "-e") == 0) {
+                if (argc < 3) {
+                        fputs("error: -e with no program specified", stderr);
+                }
+                char buffer[8192] = {0};
+                strncpy(buffer, argv[2], sizeof buffer - 1);
+                execln(buffer);
+        } else {
+                char const *file = (argc > 1 && strcmp(argv[1], "-") != 0) ? argv[1] : "/dev/stdin";
+                if (!vm_execute_file(file)) {
+                        fprintf(stderr, "%s\n", vm_error());
+                        return -1;
+                }
         }
 
         return 0;
